0a70d5cbd0bb3e0407693bf8fd5a66cf5db51c3e
Radina Georgieva <radina940729@gmail.com>
2024-02-04 12:35:47 +0200

    feat:add service function to create crop

--- farm-app/src/services/CropService.tsx
+++ farm-app/src/services/CropService.tsx
@@ -1,3 +1,5 @@
+import { CropData } from "../components/statics/interfaces";
+
 const apiUrl = "http://localhost:3000/crop";
 
 const CropService = {
@@ -14,6 +16,54 @@ const CropService = {
       return cropData;
     } catch (error) {
       console.error("Error in fetching crops", error);
+      throw error;
+    }
+  },
+  createCrop: async (cropData: CropData) => {
+    try {
+      const response = await fetch(`${apiUrl}/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(cropData),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(errorData)
+        if (response.status === 400 && errorData.statusCode === 400) {
+          throw new Error(errorData.message || "Failed to create crop");
+        }
+        throw new Error(errorData.message || "Failed to create crop");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error during crop creation:", error);
+      throw new Error("Failed to create crop");
+    }
+  },
+  deleteCropById: async (cropId: string) => {
+    try {
+      const response = await fetch(`${apiUrl}/${cropId}/soft`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete crop ");
+      }
+
+      const crop = await response.json();
+      return crop;
+    } catch (error) {
+      console.error("Error in deleting crop", error);
       throw error;
     }
   },
