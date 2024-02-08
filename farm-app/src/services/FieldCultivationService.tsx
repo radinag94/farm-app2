65033b74ffebd507ee5ff3260cb202a608441bf9
Radina Georgieva <radina940729@gmail.com>
2024-02-08 10:41:22 +0200

    feat:add new service for create field cultivation

--- farm-app/src/services/FieldCultivationService.tsx
+++ farm-app/src/services/FieldCultivationService.tsx
@@ -1,3 +1,4 @@
+import { CreateFieldCultivationFormData } from "../components/statics/interfaces";
 const apiUrl = "http://localhost:3000/field-process";
 
 const FieldCultivationService = {
@@ -14,6 +15,32 @@ const FieldCultivationService = {
       return fieldCultivationData;
     } catch (error) {
       console.error("Error in fetching field cultivations", error);
+      throw error;
+    }
+  },
+  createFieldCultivation: async (
+    fieldCultivation: CreateFieldCultivationFormData
+  ) => {
+    try {
+      const response = await fetch(`${apiUrl}/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(fieldCultivation),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log("Backend error message:", errorData.message);
+        throw new Error(errorData.message);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error during farm creation:", error);
       throw error;
     }
   },
