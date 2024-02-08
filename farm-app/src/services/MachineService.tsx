d336d85df031c835c8c3bb126d8f667a9dfae931
Radina Georgieva <radina940729@gmail.com>
2024-02-08 10:33:37 +0200

    feat: add new service functions for machines to fetch machine by farm id ,by field id

--- farm-app/src/services/MachineService.tsx
+++ farm-app/src/services/MachineService.tsx
@@ -1,3 +1,4 @@
+import { UpdateMachineFormValues } from "../components/Forms/UpdateMachineForm/UpdateMachineForm";
 import { MachineFormData } from "../components/statics/interfaces";
 const apiUrl = "http://localhost:3000/machine";
 
@@ -18,7 +19,7 @@ const MachineService = {
       throw error;
     }
   },
-  createFarm: async (machineFormData: MachineFormData) => {
+  createMachine: async (machineFormData: MachineFormData) => {
     try {
       const response = await fetch(`${apiUrl}/create`, {
         method: "POST",
@@ -30,14 +31,37 @@ const MachineService = {
       });
 
       if (!response.ok) {
-        throw new Error("Failed to create machine");
+        const errorData = await response.json();
+        console.log("Backend error message:", errorData.message);
+        throw new Error(errorData.message);
       }
 
       const data = await response.json();
       return data;
     } catch (error) {
       console.error("Error during machine creation:", error);
-      throw new Error("Failed to create machine");
+      throw error;
+    }
+  },
+  fetchMachineById: async (machineId: string) => {
+    try {
+      const response = await fetch(`${apiUrl}/${machineId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch machine details");
+      }
+
+      const machineDetails = await response.json();
+      return machineDetails;
+    } catch (error) {
+      console.error("Error in fetching machine details", error);
+      throw error;
     }
   },
   fetchMachinesByFarmId: async (farmId: string) => {
@@ -58,6 +82,96 @@ const MachineService = {
       return machines;
     } catch (error) {
       console.error("Error in fetching machines by farm ID", error);
+      throw error;
+    }
+  },
+  fetchMachinesByFieldId: async (fieldId: string) => {
+    try {
+      const response = await fetch(`${apiUrl}/byFieldId/${fieldId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch machines by field ID");
+      }
+
+      const machines = await response.json();
+      return machines;
+    } catch (error) {
+      console.error("Error in fetching machines by field ID", error);
+      throw error;
+    }
+  },
+  fetchFarmByMachineId: async (machineId: string) => {
+    try {
+      const response = await fetch(`${apiUrl}/byMachineId/${machineId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch farm by machine ID");
+      }
+
+      const farm = await response.json();
+      return farm;
+    } catch (error) {
+      console.error("Error in fetching farm by machine ID", error);
+      throw error;
+    }
+  },
+  updateMachine: async (
+    machineId: string,
+    machineFormData: UpdateMachineFormValues
+  ) => {
+    try {
+      const response = await fetch(`${apiUrl}/${machineId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(machineFormData),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log("Backend error message:", errorData.message);
+        throw new Error(errorData.message);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error during machine update:", error);
+      throw error;
+    }
+  },
+  deleteMachineById: async (machineId: string) => {
+    try {
+      const response = await fetch(`${apiUrl}/${machineId}/soft`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete machine details and machine ");
+      }
+
+      const machine = await response.json();
+      return machine;
+    } catch (error) {
+      console.error("Error in deleting machine details", error);
       throw error;
     }
   },
