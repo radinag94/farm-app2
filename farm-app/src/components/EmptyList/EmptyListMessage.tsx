d57eaf1c53d31d1fe1ce2fa7e261e5b54ca301b1
Radina Georgieva <radina940729@gmail.com>
2024-02-12 12:27:52 +0200

    feat:add empty list message component(reusable)

--- farm-app/src/components/EmptyList/EmptyListMessage.tsx
+++ farm-app/src/components/EmptyList/EmptyListMessage.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import { EmptyListProps } from "../statics/interfaces";
 import { EmptyListContainer } from "./EmptyListMessage.style";
-
-
 
 const EmptyList: React.FC<EmptyListProps> = ({ message }) => {
   return (
